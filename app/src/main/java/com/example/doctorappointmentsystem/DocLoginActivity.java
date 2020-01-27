4f701bb55374de043203e7342927602c0d7fa7bc
bvekxettri <bvekxettri@gmail.com>
2020-01-27 12:04:26 +0545

    Login and registration UI

---
+++ app/src/main/java/com/example/doctorappointmentsystem/DocLoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.doctorappointmentsystem;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class DocLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_doc_login);
+    }
+}

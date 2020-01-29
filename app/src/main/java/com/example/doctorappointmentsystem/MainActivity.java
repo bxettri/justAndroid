d906967f95358a16785220aae973ace7f8201550
bvekxettri <bvekxettri@gmail.com>
2020-01-29 12:57:54 +0545

    Nav logic

--- app/src/main/java/com/example/doctorappointmentsystem/MainActivity.java
+++ app/src/main/java/com/example/doctorappointmentsystem/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.doctorappointmentsystem;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
@@ -8,8 +9,14 @@ import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 import android.view.Gravity;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.doctorappointmentsystem.Fragment.AppointmentFragment;
+import com.example.doctorappointmentsystem.Fragment.MessageFragment;
+import com.example.doctorappointmentsystem.Fragment.ProfileFragment;
+import com.google.android.material.navigation.NavigationView;
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
 
@@ -22,12 +29,21 @@ public class MainActivity extends AppCompatActivity {
 
 
         drawer=findViewById(R.id.drawyer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
 
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
                 R.string.navigation_drawer_open, R.string.navigation_drawer_close);
 
         drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        if(savedInstanceState==null){
+        getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                new MessageFragment()).commit();
+        navigationView.setCheckedItem(R.id.nav_message);
+        }
+
     }
 
     public void onBackPressed(){
@@ -36,5 +52,29 @@ public class MainActivity extends AppCompatActivity {
         }else{
             super.onBackPressed();
         }
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+        switch (item.getItemId()){
+            case R.id.nav_message:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new MessageFragment()).commit();
+                break;
+
+            case R.id.nav_appointment:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new AppointmentFragment()).commit();
+                break;
+
+            case R.id.nav_profile:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ProfileFragment()).commit();
+                break;
+
+
+        }
+        return true;
     }
 }
